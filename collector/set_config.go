368d883dc41a0725dda30ac357b1f51adcfb2563
anushree-n <anushree.bnp@gmail.com>
2015-06-19 14:27:01 -0700

    Update set_config.go

--- collector/set_config.go
+++ collector/set_config.go
@@ -1,10 +1,23 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package collector
 
 import (
-	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"time"
-	"encoding/json"
 )
 
 type Collector struct {
@@ -15,23 +28,21 @@ type Collector struct {
 }
 
 type Config struct {
-	Endpoint      string         "json:'endpoint'"
-	MetricsConfig []metricConfig "json:'metrics'"
+	Endpoint      string         `json:"endpoint"`
+	MetricsConfig []metricConfig `json:"metricsConfig"`
 }
 
 type metricConfig struct {
-	Name             string "json:'name'"
-	MetricType       string "json:'metricType'"
-	Units            string "json:'units'"
-	PollingFrequency string "json:'pollingFrequency'"
-	Regex            string "json:'regex'"
+	Name             string `json:"name"`
+	MetricType       string `json:"metricType"`
+	Units            string `json:"units"`
+	PollingFrequency string `json:"pollingFrequency"`
+	Regex            string `json:"regex"`
 }
 
 func SetCollectorConfig(collector *Collector, file string) error {
-	fmt.Println("In setconfig")
 	configFile, err := ioutil.ReadFile(file)
 	if err != nil {
-		fmt.Println( err )
 		collector.err = err
 	} else {
 		var configInJSON Config
@@ -47,4 +58,3 @@ func SetCollectorConfig(collector *Collector, file string) error {
 
 	return collector.err
 }
-
