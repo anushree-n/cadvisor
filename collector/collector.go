c0f2e1d45872924065a7bc1916721c24759f6eff
anushree-n <anushreen@google.com>
2015-06-23 11:42:28 -0700

    Implement Collect() in collector.go

--- collector/collector.go
+++ collector/collector.go
@@ -16,8 +16,13 @@ package collector
 
 import (
 	"time"
+	"net/http"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"strconv"
 
-	"github.com/google/cadvisor/info/v2"
+        "github.com/google/cadvisor/info/v2"
 )
 
 func NewNginxCollector() (*Collector, error) {
@@ -32,6 +37,52 @@ func (collector *Collector) Name() string {
 }
 
 func (collector *Collector) Collect() (time.Time, []v2.Metric, error) {
-	//TO BE IMPLEMENTED
-	return time.Now(), nil, nil
+ 	currentTime := time.Now()
+	collector.nextCollectionTime = currentTime.Add(time.Duration(10 * time.Second))
+
+        uri := "http://"+collector.configFile.Endpoint
+        uri = strings.Replace(uri, "host", "localhost", 1)
+        uri = strings.Replace(uri, "port", "8000", 1)
+
+	response, err := http.Get(uri)
+	if err != nil {
+		return collector.nextCollectionTime, nil, err
+	}
+
+	pageContent, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return collector.nextCollectionTime, nil, err
+	}
+
+	lines := strings.Split(string(pageContent), "\n")
+
+	var metricType v2.MetricType
+	var regex *regexp.Regexp
+
+	numOfMetrics := len(collector.configFile.MetricsConfig)
+	metrics := make([]v2.Metric, numOfMetrics)
+	
+	for ind, metricConfig := range collector.configFile.MetricsConfig {
+		metrics[ind].Name = metricConfig.Name
+		metrics[ind].Type = metricType
+		metrics[ind].Labels = nil
+		metrics[ind].FloatPoints = nil
+
+		metricValue := make([]v2.IntPoint, 1)
+		regex = regexp.MustCompile(metricConfig.Regex)
+
+		for _, line := range lines {
+			matchString := regex.FindStringSubmatch(line)
+			if matchString != nil {
+				regVal, _ := strconv.ParseInt(strings.Trim(matchString[1], " "), 10, 64)
+				metricValue[0].Value = regVal
+				metricValue[0].Timestamp = time.Now()
+
+				metrics[ind].IntPoints = metricValue
+				break
+			}
+		}
+	}
+
+	return collector.nextCollectionTime, metrics, nil
 }
